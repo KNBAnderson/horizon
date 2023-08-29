b0002332da42347dbaaa6bdb801889c795179cc4
Katlin Anderson <katlin.nbanderson@gmail.com>
2023-08-29 14:14:02 -0700

    Initial commit with starter code

--- src/main/starter/browser.cljs
+++ src/main/starter/browser.cljs
@@ -1,8 +1,27 @@
-(ns starter.browser)
+(ns starter.browser
+  (:require [reagent.core :as r]
+            [reagent.dom :as rd]))
+
+(defonce state (r/atom {:items ["Hello" "World!"]}))
+
+(defn- new-item []
+  [:input
+   {:type "text"
+    :placeholder "Enter a new item"
+    :on-key-down (fn [e]
+                   (when (= "Enter" (.-key e))
+                     (swap! state update :items conj (.. e -target -value))))}])
+
+(defn- hello-world []
+  [:div
+   [new-item]
+   [:ul (map (fn [item]
+               [:li {:key item} item])
+             (:items @state))]])
 
 ;; start is called by init and after code reloading finishes
 (defn ^:dev/after-load start []
-  (js/console.log "start"))
+  (rd/render [hello-world] (js/document.getElementById "app")))
 
 (defn init []
   ;; init is called ONCE when the page loads
@@ -14,3 +33,4 @@
 ;; this is called before any code is reloaded
 (defn ^:dev/before-load stop []
   (js/console.log "stop"))
+
